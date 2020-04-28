5c31fb679a4b08c82dc5ad2fd76678162a6beb00
jtorresli <manu.jose.torres@gmail.com>
2020-04-28 16:15:57 -0500

    feat: quitando las celulas muertas v1

--- Practica 1/ejercicio1.py
+++ Practica 1/ejercicio1.py
@@ -1,47 +1,25 @@
-'''
-import cv2
-import numpy as np
-from matplotlib import pyplot as plt
 
-BLUE = [255,0,0]
-
-img1 = cv2.imread('1.png')
-
-replicate = cv2.copyMakeBorder(img1,10,10,10,10,cv2.BORDER_REPLICATE)
-reflect = cv2.copyMakeBorder(img1,10,10,10,10,cv2.BORDER_REFLECT)
-reflect101 = cv2.copyMakeBorder(img1,10,10,10,10,cv2.BORDER_REFLECT_101)
-wrap = cv2.copyMakeBorder(img1,10,10,10,10,cv2.BORDER_WRAP)
-constant= cv2.copyMakeBorder(img1,10,10,10,10,cv2.BORDER_CONSTANT,value=BLUE)
-
-plt.subplot(231),plt.imshow(img1,'gray'),plt.title('ORIGINAL')
-plt.subplot(232),plt.imshow(replicate,'gray'),plt.title('REPLICATE')
-plt.subplot(233),plt.imshow(reflect,'gray'),plt.title('REFLECT')
-plt.subplot(234),plt.imshow(reflect101,'gray'),plt.title('REFLECT_101')
-plt.subplot(235),plt.imshow(wrap,'gray'),plt.title('WRAP')
-plt.subplot(236),plt.imshow(constant,'gray'),plt.title('CONSTANT')
-
-plt.show()
-'''
 import numpy as np
 import cv2 as cv
 from matplotlib import pyplot as plt
 
-img = cv.imread('1.png', 0)
+original = cv.imread('thresh1.png', 0)
 
-px = img[100,100]
+plt.hist(original.ravel(),256,[0,256]); plt.show()
 
-width = img.shape[1]
-height = img.shape[0]
+width = original.shape[0]
+height = original.shape[1]
 
-plt.hist(img.ravel(),256,[0,256]); plt.show()
-'''
+resultado = np.zeros((width, height, 1),np.uint8)
+
 for i in range(height):
-	for j in range(width):
-		print (img[i,j], ' = ', img[i,j][0])
+	for j in range(width-1):
+		if original[i-1,j-1]>100 and original[i-1,j-1]<170:
+			resultado[i-1,j-1] = 0 
 
-cv.imshow('ImageWindow', img)
+cv.imshow('ImageWindow',resultado)
 cv.waitKey(0) 
 cv.destroyAllWindows()
 cv.waitKey(1) 
 exit()
-'''
+
