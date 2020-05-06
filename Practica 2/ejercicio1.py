b52c09719cfba5cc5a7e22864a4f491d38f84807
jtorresli <manu.jose.torres@gmail.com>
2020-05-06 11:53:44 -0500

    feat: mostrando histograma de contraste y ajustando parametros

--- Practica 2/ejercicio1.py
+++ Practica 2/ejercicio1.py
@@ -1,41 +1,57 @@
-import cv2
+
 import sys
 import cv2 as cv
 import numpy as np
 from matplotlib import pyplot as plt
 
-img = cv2.imread('contr2.jpg', cv2.IMREAD_GRAYSCALE)
-cv2.imshow('Original Imagen', img)
+# loading image
+img = cv.imread('contr2.jpg', cv.IMREAD_GRAYSCALE)
+cv.imshow('Original Imagen', img)
 
 if img.size == 0:
     sys.exit("Error: the image has not been correctly loaded.")
 
-
-hist = cv2.calcHist([img], [0], None, [256], [0, 256])
+# Generating histogram
+hist = cv.calcHist([img], [0], None, [256], [0, 256])
 plt.plot(hist, color='gray' )
-plt.title("Histograma  of contr2") 
+plt.title("Histograma  of image") 
 plt.xlabel('intensidad de iluminacion')
 plt.ylabel('cantidad de pixeles')
-plt.savefig('histogram1.png')
+plt.savefig('histogram1_1.png')
 plt.show()
 
-
+# Dimensions
 heigth = img.shape[0]
 width = img.shape[1]
 
+# Creating matrix
 Contrast = np.zeros((heigth, width, 1),np.uint8)
-a=0
-b=255
-c=55
-d=135
+
+# Contrast Stretching
+a = 0
+b = 255
+c = 50
+d =	144
+
+temp = (b - a)/(d - c)
+
 for x in range(0, heigth, 1):
     for y in range(0, width, 1):
-    	aux = (img[x][y] - c)
-    	Contrast[x][y] = aux * ((b-a)/(d-c)) + a 
+    	Contrast[x][y] = (img[x][y] - c) * temp + a 
+
 cv.imshow('Contrast',Contrast)
+
+hist = cv.calcHist([Contrast], [0], None, [256], [0, 256])
+plt.title("Histograma  of Contrast") 
+plt.plot(hist, color='gray' )
+plt.xlabel('lighting intensity')
+plt.ylabel('number of pixels')
+plt.savefig('histogram1_2.png')
+plt.show()
 
 filename = 'resultado1.png'
 
+# Saving the image 
 cv.imwrite(filename, Contrast) 
 
 print ("Presione una tecla para cerrar")
@@ -43,8 +59,3 @@ cv.waitKey(0)
 cv.destroyAllWindows()
 cv.waitKey(1) 
 exit()
-
-cv2.waitKey(0)
-
-
-cv2.destroyAllWindows()
