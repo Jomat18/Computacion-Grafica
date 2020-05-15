bda162f2b95780c34fb2faaf2718d5aa04a9a834
Erika Sejje <esejjec@unsa.edu.pe>
2020-05-15 07:45:03 -0500

    mejora ejercicio 3

--- Practica 4/ejercicio3.py
+++ Practica 4/ejercicio3.py
@@ -9,11 +9,11 @@ image = cv.imread('log_12.jpg', 0)
 cv.imshow('Original Imagen',image)
 
 # Generar su histograma
-plt.title("Histograma  of log_1.jpg") 
+plt.title("Histograma  of log_12.jpg") 
 plt.hist(image.ravel(),256,[0,256]) 
 plt.xlabel('lighting intensity')
 plt.ylabel('number of pixels')
-plt.savefig('histogram1.png')
+plt.savefig('histogramlog_12.png')
 plt.show()
 
 # Dimensiones de la imagen original
@@ -25,24 +25,27 @@ Logarithm = np.zeros((heigth, width, 1),np.uint8)
 
 
 #aplicamos Logarithm Operator
-c = 100
+c = 120
 for x in range(0, heigth, 1):
     for y in range(0, width, 1):
-    	Logarithm[x][y] = c * math.sqrt( 1 + image[x][y] )
-
-
+    	aux = c * math.sqrt( 1 + image[x][y] )
+    	if (aux > 255):
+    		aux = 255
+    	if (aux < 0):
+    		aux = 0
+    	Logarithm[x][y] = aux
 
 #calculamos el histograma de nuestro resultado
 plt.title("Histograma  of resul log_1.jpg") 
 plt.hist(Logarithm.ravel(),256,[0,256]) 
 plt.xlabel('lighting intensity')
 plt.ylabel('number of pixels')
-plt.savefig('histogramOfResult1.png')
+plt.savefig('histogramOfResultlog_12.jpg')
 plt.show()
 
 # Guardamos la imagen
 cv.imshow('Logarithm',Logarithm)
-filename = 'resultado1-70.png'
+filename = 'resultadolog_12.jpg'
 cv.imwrite(filename, Logarithm) 
 
 cv.waitKey(0)
