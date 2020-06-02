cd498c66a52b78cbeaff2bc80468cb83d1348afe
zulmarina1687 <luzmarinapilco@gmail.com>
2020-06-01 22:20:38 -0500

    ejercicio1 p6

--- Practica 6/ejercicio1.py
+++ Practica 6/ejercicio1.py
@@ -1,57 +1,64 @@
+import sys
 import cv2
 import numpy as np 
 import matplotlib as plt
-
 
 def sumaN(img1, img2):
 	# Dimensions
 	heigth = img2.shape[0]
 	width = img2.shape[1]
-
+	if img1.shape[0]>img2.shape[0]:
+		heigth = img2.shape[0]
+	if img1.shape[1]>img2.shape[1]:
+		width = img2.shape[1] 
 	# Creating matrix
-	img_out = np.zeros((heigth, width, 1),np.uint8)
-
-	s1 = lambda x: x/2
-	img1 = s1(img1)
-	img2 = s1(img2)
-	
+	img_out = np.zeros((heigth, width, 3), np.int)
 	for x in range(0, heigth, 1):
 	    for y in range(0, width, 1):
-	        img_out[x][y] = img1[x][y] + img2[x][y] 
-	#img_out = img1 + img2
-
-
+	        img_out[x][y] = img1[x][y] + img2[x][y]
 	return img_out
 
-def sumaC(img1, img2):
+def sumaS(img1, img2):
 	# Dimensions
 	heigth = img2.shape[0]
 	width = img2.shape[1]
-
+	if img1.shape[0]>img2.shape[0]:
+		heigth = img2.shape[0]
+	if img1.shape[1]>img2.shape[1]:
+		width = img2.shape[1] 
 	# Creating matrix
-	img_out = np.zeros((heigth, width, 1),np.uint8)
-
+	img_out = np.zeros((heigth, width, 3), np.int)
 	s1 = lambda x: x/2
 	img1 = s1(img1)
 	img2 = s1(img2)
-	
-	#for x in range(0, heigth, 1):
-	#    for y in range(0, width, 1):
-	#        img_out[x][y] = img1[x][y] + img2[x][y] 
-	#img_out = img1 + img2
+	for x in range(0, heigth, 1):
+	    for y in range(0, width, 1):
+	        img_out[x][y] = img1[x][y] + img2[x][y]
+	return img_out
 
-
+def sumaC(img1, c):
+	# Dimensions
+	heigth = img1.shape[0]
+	width = img1.shape[1]
+	# Creating matrix
+	img_out = np.zeros((heigth, width, 3), np.int)
+	for x in range(0, heigth, 1):
+	    for y in range(0, width, 1):
+	        img_out[x][y] = img1[x][y] + c
 	return img_out
 
 
 if __name__ == "__main__":
-	img1 = cv2.imread("add_1.jpg")
-    img2 = cv2.imread("add_2.jpg")
-    img_out = sumaC(img1,img2)
-    cv2.imwrite("resul.jpg",img_out)
+	filename1 = sys.argv[1]
+	filename2 = sys.argv[2]
+	img1 = cv2.imread(filename1)
+	img2 = cv2.imread(filename2)
+	img_out = sumaN(img1,img2)
+	cv2.imwrite("resultado1N.jpg",img_out)
+	img_out = sumaS(img1,img2)
+	cv2.imwrite("resultado1S.jpg",img_out)
+	cv2.destroyAllWindows()
+	cv2.waitKey(1)
+	exit()
 
-    #img1 = cv2.imread("add_1.jpg",0)
-    #img2 = cv2.imread("add_2.jpg",0)
-    #img_out = suma(img1,img2)
-    #cv2.imwrite("resul.jpg",img_out)
 
