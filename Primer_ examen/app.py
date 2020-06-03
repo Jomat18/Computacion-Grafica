dc781195e0f98de51bf395653083bd2de1ed955b
jtorresli <manu.jose.torres@gmail.com>
2020-06-02 23:10:18 -0500

    feat: actualizando app.py

--- Primer_ examen/app.py
+++ Primer_ examen/app.py
@@ -5,6 +5,7 @@ from werkzeug.utils import secure_filename
 app = Flask(__name__)
 dir = os.path.dirname(os.path.realpath(__file__))
 filename = ''
+filename2 = ''
 
 @app.route("/")
 def home():
@@ -12,7 +13,10 @@ def home():
 
 @app.route("/mostrar", methods=["POST"])
 def mostrar(): 
-    global filename           
+    global filename
+    global filename2
+                   
+    filename2 = filename
     try:
         if 'file' in request.files:
             imageFile = request.files['file']
@@ -26,41 +30,28 @@ def mostrar():
 @app.route("/calcular", methods=["POST"])        
 def calcular():
     global filename
-    valor_t1 = request.form['valor_t1']
-    valor_t2 = request.form['valor_t2']
-    valor_minimo = request.form['valor_minimo']
-    valor_maximo = request.form['valor_maximo']
-    #valor_a = request.form['valor_a']
-    valor_b = request.form['valor_b']
-    valor_c = request.form['valor_c']
-    valor_c_exp = request.form['valor_c_exp']
-    valor_c_rai = request.form['valor_c_rai']
-    #valor_d = request.form['valor_d']
+    valor_1 = request.form['valor_1']
+    valor_2 = request.form['valor_2']
     valor_r = request.form['valor_r']
-    valor_intensidad = request.form['valor_intensidad']
     operador = request.form['operador']
 
-    #os.system('python algoritmos/thresholding.py static/images/'+filename + ' '+valor_a+ ' '+valor_b)
+    if operador=='equalizacion':
+        os.system('python algoritmos/hist_Equalization.py static/images/'+filename)
+    elif operador=='logaritmo':
+        os.system('python algoritmos/logaritmo.py static/images/'+filename +' '+ valor_1)
+    elif operador=='exponencial':    
+        os.system('python algoritmos/exponencial.py static/images/'+filename+' '+valor_1+' '+valor_2)
+    elif operador=='raizC':    
+        os.system('python algoritmos/raizC.py static/images/'+filename+' '+valor_1+' '+valor_r)
+    elif operador=='contrast':    
+        os.system('python algoritmos/contrast.py static/images/'+filename+' '+valor_1)
+    elif operador=='adicion':    
+        os.system('python algoritmos/adicion.py static/images/'+filename+' static/images/'+filename2)    
+    else:    
+        os.system('python algoritmos/thresholding.py static/images/'+filename+' '+valor_1+' '+valor_2)
 
-    # Aqui llamar a las funciones
-    if operador == "thresholding":
-       os.system('python algoritmos/thresholding.py static/images/'+filename + ' '+valor_t1+ ' '+valor_t2)
-    elif operador == "contrast":
-       os.system('python algoritmos/contrast.py static/images/'+filename + ' '+valor_minimo+' '+valor_maximo)
-    elif operador == "ecualizacion":
-       os.system('python algoritmos/ecualizacion.py static/images/'+filename + ' '+valor_intensidad)
-    elif operador == "logaritmico":
-       os.system('python algoritmos/logaritmico.py static/images/'+filename + ' '+valor_c)
-    elif operador == "raiz":
-       os.system('python algoritmos/raiz.py static/images/'+filename + ' '+valor_c)
-    elif operador == "exponencial":
-       os.system('python algoritmos/exponencial.py static/images/'+filename + ' '+valor_c_exp+ ' '+valor_b)
-    else:
-       os.system('python algoritmos/raisetopower.py static/images/'+filename + ' '+valor_c_rai+ ' '+valor_r)
-
-    #os.system('python ejercicio1.py static/images/'+filename + ' '+valor_c)
     return jsonify(name = filename)
 
 
 if __name__ == '__main__':
-    app.run(port=5000,debug=True)
+    app.run(debug = True, port=5000)
