34ec888dd67385db5678302db3454c49a98ed76f
Erika Sejje <esejjec@unsa.edu.pe>
2020-06-04 19:26:57 -0500

    ejercicio 1

---
+++ Practica 7/ejercicio1.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+
+import sys
+import cv2 as cv
+import numpy as np
+
+
+#multiplicación por una constante
+
+def MultiplcacionC(img1, c):
+
+    heigth = img1.shape[0]
+    width = img1.shape[1]
+
+    # Imagen resultado
+    nueva_imagen = np.zeros((heigth, width, 3), np.int)
+
+    for i in range(heigth):
+        for j in range(width):
+            for k in range(3):
+                nueva_imagen[i][j][k] = img1[i][j][k] * c
+
+    return nueva_imagen
+
+if __name__ == "__main__":
+
+    filename1 = sys.argv[1]
+    c = int(sys.argv[2])
+
+    # Leer imagenes
+    img1 = cv.imread(filename1)
+
+    nueva_imagen = MultiplcacionC(img1,c)
+
+    # Guardando la imagen del resultado
+    cv.imwrite('resultado_1_'+str(c)+'.jpg', nueva_imagen) 
+    
+    cv.destroyAllWindows()
+    cv.waitKey(1) 
+    exit()
