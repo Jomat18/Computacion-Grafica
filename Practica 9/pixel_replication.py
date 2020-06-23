053a64943bfec602e4d75c826f1fa91d74a3777e
jtorresli <manu.jose.torres@gmail.com>
2020-06-23 14:22:54 -0500

    feat: replicacion de pixeles

---
+++ Practica 9/pixel_replication.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+
+import sys
+import cv2 
+import numpy as np 
+from matplotlib import pyplot as plt
+
+def pixel_replication(img):
+
+    heigth, width, rgb = img.shape
+
+    scaling = np.zeros((heigth*2, width*2, rgb), np.uint8)
+
+    for row in range(heigth):
+        for column in range(width):
+            scaling[row*2][column*2] = img[row][column]
+            scaling[row*2][column*2+1] = img[row][column]
+            scaling[row*2+1][column*2] = img[row][column]
+            scaling[row*2+1][column*2+1] = img[row][column]
+            
+
+    plt.subplot(2, 2, 1) 
+    plt.title('Original') 
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img = np.array(img)
+    plt.imshow(img)
+    plt.subplot(2, 2, 2) 
+    plt.title('Scaling') 
+    scaling = cv2.cvtColor(scaling, cv2.COLOR_BGR2RGB)
+    scaling = np.array(scaling)
+    plt.imshow(scaling)
+    plt.show()        
+    cv2.imwrite('pixel_replication.jpg', scaling)
+
+
+if __name__ == "__main__":
+
+    filename = sys.argv[1]
+
+    img = cv2.imread(filename)
+
+    pixel_replication(img)
+    
+    cv2.destroyAllWindows()
+    cv2.waitKey(1) 
+    exit()
