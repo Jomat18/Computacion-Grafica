a6e4896e1dff6e27c1e7f5c9bea3e4865531140d
jtorresli <manu.jose.torres@gmail.com>
2020-08-18 09:09:51 -0500

    feat: agregando pruebas

--- Tercer_examen/main.py
+++ Tercer_examen/main.py
@@ -3,18 +3,26 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-MIN_MATCH_COUNT = 10
+MIN_MATCH_COUNT = 10 #25
 
-#Cargar diferentes pares de imagenes
+# Cargar diferentes pares de imagenes
 img_pairs = [("imagen1.jpg", "imagen2.jpg"),
              ("imagen2.jpg", "imagen3.jpg"),
              ("imagen1.jpg", "imagen3.jpg")]
+
+#img_pairs = [("viff.000.ppm","viff.001.ppm"),
+#             ("viff.001.ppm","viff.002.ppm"),
+#             ("viff.000.ppm","viff.002.ppm")]
 counter = 0
 
-#Reemplazar K con la matriz intrinsica
-K = np.array([[518.86, 0., 285.58],
-              [0., 519.47, 213.74],
-              [0.,   0.,   1.]])
+# Creando la matriz intrinseca K
+# E = K'*F*K
+# K es la matriz de la camara equilibrada, F la matriz fundamental y E la matriz esencial
+K = np.array([[518.86, 0., 285.58],   #[ fm_x   0     C_x] 
+              [0., 519.47, 213.74],   #[ 0      fm_y  C_y]
+              [0.,   0.,   1.]])      #[ 0      0      1 ]
+
+point_imagen = []
 
 for img_pair_1, img_pair_2 in img_pairs:
     counter += 1
@@ -22,12 +30,30 @@ for img_pair_1, img_pair_2 in img_pairs:
     img2=cv2.imread(img_pair_2)
 
     ###1 - SIFT
-    #Detectar las caracteristicas para ambas imagenes
-    sift = cv2.xfeatures2d.SIFT_create()
+    #Detectar las caracteristicas o keypoints  y descriptores para ambas imagenes
+    sift = cv2.xfeatures2d.SIFT_create() 
+
     kp1, des1 = sift.detectAndCompute(img1,None)
     kp2, des2 = sift.detectAndCompute(img2,None)
 
-    #usar flann para realizar la coincidencia de caracteriticas
+    # Dibujar los keypoints de las imagenes
+    img_1=img1.copy()
+    img_2=img2.copy() 
+    img_1=cv2.drawKeypoints(img_1,kp1,img_1,color=(0,255,0)) 
+    img_2=cv2.drawKeypoints(img_2,kp2,img_2,color=(0,255,0))
+
+    img_1=cv2.resize(img_1, (1200, 800))
+    plt.imshow(img_1)
+    plt.show()
+
+    img_2=cv2.resize(img_2, (1200, 800))
+    plt.imshow(img_2)
+    plt.show()
+
+    #keypoint,w1=des1.shape
+    #keypoint,w2=des2.shape
+
+    # Usar flann para realizar la coincidencia de caracteriticas
     FLANN_INDEX_KDTREE = 0
     index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
     search_params = dict(checks = 50)
@@ -36,61 +62,82 @@ for img_pair_1, img_pair_2 in img_pairs:
 
     matches = flann.knnMatch(des1,des2,k=2)
 
-    #Almacenar todas coincidencias con el ratio propuesto por Lowe 0.7
+    # Las parejas con menor distancia son las que queremos.
+    #matches = sorted(matches, key = lambda x : x.distance)  #matches[:50]
+
+    # Almacenar todas coincidencias con el ratio propuesto por Lowe 0.7
     good = []
     for m,n in matches:
-        if m.distance < 0.7 * n.distance:
+        if m.distance < 0.7 * n.distance:  #H/F
             good.append(m)
 
+
+    # recuperación de las coordenadas de la imagen de puntos clave coincidentes
     if len(good)>MIN_MATCH_COUNT:
         p1 = np.float32([ kp1[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
         p2 = np.float32([ kp2[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
 
-    draw_params = dict(matchColor = (0,255,0), # draw matches in green color
-                       singlePointColor = None,
+
+    print ("Numero de Matches")
+    print (len(good))    
+
+    draw_params = dict(matchColor = (0,255,0), # color verde
+                       singlePointColor = (0,255,0),
                        flags = 2)
 
+    
     img_siftmatch = cv2.drawMatches(img1,kp1,img2,kp2,good,None,**draw_params)
     cv2.imwrite('sift_match_' + str(counter) + '.png',img_siftmatch)
 
-    #2 - Matriz esencial
+    img_siftmatch_display = cv2.resize(img_siftmatch, (1200,800))
+    plt.imshow(img_siftmatch_display)
+    plt.show()
+
+    #2 - Matriz esencial    
+    # Ransac elimina las correspondencias atípicas
     E, mask = cv2.findEssentialMat(p1, p2, K, cv2.RANSAC, 0.999, 1.0);
 
     matchesMask = mask.ravel().tolist()
+  
 
     draw_params = dict(matchColor = (0,255,0),
                        singlePointColor = None,
                        matchesMask = matchesMask,
                        flags = 2)
 
+    # emparejamiento interno, correspondencias de características internas
     img_inliermatch = cv2.drawMatches(img1,kp1,img2,kp2,good,None,**draw_params)
     cv2.imwrite('inlier_match_' + str(counter) + '.png',img_inliermatch)
-    print("Essential matrix:")
+    print("Matriz Esencial:")
     print(E)
 
     #3 - Recuperar puntos
 
+    # K_normal=[[1.0,0.0,0.0,0.0,1.0,0.0,0,0,1]]
+    # K_normal=np.array(K).reshape(3,3)
+
+    # Decomposing E to get R and T, Reconstruccion inicial
     points, R, t, mask = cv2.recoverPose(E, p1, p2)
-    print("Rotation:")
+    print("Matriz de Rotacion:")
     print(R)
-    print("Translation:")
+    print("Matriz de Translacion:")
     print(t)
-    # p1_tmp = np.expand_dims(np.squeeze(p1), 0)
+
     p1_tmp = np.ones([3, p1.shape[0]])
     p1_tmp[:2,:] = np.squeeze(p1).T
     p2_tmp = np.ones([3, p2.shape[0]])
     p2_tmp[:2,:] = np.squeeze(p2).T
-    print((np.dot(R, p2_tmp) + t) - p1_tmp)
+    #print((np.dot(R, p2_tmp) + t) - p1_tmp)
 
-    #4 - Triangulacion
-    #Calcular la matriz de proyeccion para ambas camaras
+    #4 - Triangulacion linear
+    # Calculamos la matriz de Proyecciones para ambas camaras
     M_r = np.hstack((R, t))
     M_l = np.hstack((np.eye(3, 3), np.zeros((3, 1))))
 
     P_l = np.dot(K,  M_l)
     P_r = np.dot(K,  M_r)
 
-    #Puntos no distorsionados
+    #Puntos no distorsionados 
     p1 = p1[np.asarray(matchesMask)==1,:,:]
     p2 = p2[np.asarray(matchesMask)==1,:,:]
     p1_un = cv2.undistortPoints(p1,K,None)
@@ -98,7 +145,8 @@ for img_pair_1, img_pair_2 in img_pairs:
     p1_un = np.squeeze(p1_un)
     p2_un = np.squeeze(p2_un)
 
-    #Triangular puntos esto requiere puntos en coordenadas normalizadas
+    # Triangular puntos esto requiere puntos en coordenadas normalizadas
+    # triangulatePoints es un método puro basado en SVD.
     point_4d_hom = cv2.triangulatePoints(P_l, P_r, p1_un.T, p2_un.T)
     point_3d = point_4d_hom / np.tile(point_4d_hom[-1, :], (4, 1))
     point_3d = point_3d[:3, :].T
@@ -112,7 +160,8 @@ for img_pair_1, img_pair_2 in img_pairs:
     ax.set_zlabel('Z Label')
 
     for x, y, z in point_3d:
-        ax.scatter(x, y, z, c="r", marker="o")
+        ax.scatter(x, y, z, c="b", marker="o")
 
-    plt.show()
-    fig.savefig('3-D' + str(counter) + '.jpg')
+    plt.show()    
+
+    print ()
---
+++ Tercer_examen/main2.py
@@ -0,0 +1,148 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+#Cargar diferentes pares de imagenes
+img_pairs = [("imagen1.jpg", "imagen2.jpg"),
+             ("imagen2.jpg", "imagen3.jpg"),
+             ("imagen1.jpg", "imagen3.jpg")]
+
+
+for img_pair_1, img_pair_2 in img_pairs:    
+
+    img1 = cv2.imread(img_pair_1)        
+    img2 = cv2.imread(img_pair_2)        
+
+    sift = cv2.xfeatures2d.SIFT_create()
+
+    # Número de puntos a igualar
+    top_matches = 800
+
+    # Encuentrar los puntos clave y descriptores con SIFT
+    kp1, des1 = sift.detectAndCompute(img1,None)
+    kp2, des2 = sift.detectAndCompute(img2,None)
+
+
+    # Dibujar puntos clave
+    img_1=cv2.drawKeypoints(img1,kp1,img1,color=(0,255,0),flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+    img_2=cv2.drawKeypoints(img2,kp2,img2,color=(0,255,0),flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
+    # Mostrar los descriptores de puntos clave
+    img_1=cv2.resize(img_1, (1200, 800))
+    plt.imshow(img_1)
+    plt.show()
+
+    img_2=cv2.resize(img_2, (1200, 800))
+    plt.imshow(img_2)
+    plt.show()
+
+    # Número de descriptores de puntos clave en las imagenes
+    keypoint,w1=des1.shape
+    keypoint,w2=des2.shape
+    print ("Numero de keypoint")
+    print (keypoint)
+
+    # BFMatcher con parametros por defecto Brute-Force Matcher
+    bf = cv2.BFMatcher()
+    matches = bf.knnMatch(des1,des2, k=2)
+    #print (matches)
+
+    # Almacenar todas coincidencias con el ratio propuesto por Lowe 0.7
+    good = []
+    for m,n in matches:
+            if m.distance < 0.7*n.distance:
+                    good.append(m)
+
+
+    # Ordenando según el concepto de distancia mínima en orden ascendente
+    topMatches=sorted(good,key=lambda x:x.distance)
+
+    # Crear un mapa para las mejores coincidencias 
+    # después de que se establezca el criterio de buena coincidencia según Lowe.
+    img_match = cv2.drawMatches(img1,kp1,img2,kp2,topMatches[:top_matches],None,flags=2)
+
+    # Mostrando
+    image_match_display = cv2.resize(img_match, (1200,800))
+    plt.imshow(image_match_display)
+    plt.show()
+
+    # Elegir solo las mejores opciones
+    pts1 = np.float32([ kp1[m.queryIdx].pt for m in topMatches[:top_matches] ]).reshape(-1,1,2)
+    pts2 = np.float32([ kp2[m.trainIdx].pt for m in topMatches[:top_matches] ]).reshape(-1,1,2)
+
+    #pts1 = np.float32([ kp1[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
+    #pts2 = np.float32([ kp2[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
+
+    #print pts2
+
+    #print pts1.shape
+
+    # Creación de la matriz intrínseca K
+    K=[[1229.0,0.0,360.0,0.0,640.0,1153.0,0,0,1]]
+    K=np.array(K).reshape(3,3)
+    #print K
+
+    # puntos clave que no distorsionan para considerar las distorsiones de la cámara
+    upts1 = cv2.undistortPoints(pts1, K, distCoeffs=None)
+    upts2 = cv2.undistortPoints(pts2, K, distCoeffs=None)
+
+    # Calculando la matrix esencial
+    E, mask = cv2.findEssentialMat(pts1,pts2,1229.0,(360.0, 640.0),cv2.RANSAC,0.999,1.0)
+    print ("Matriz Esencial")
+    print (E)
+
+    K_normal=[[1.0,0.0,0.0,0.0,1.0,0.0,0,0,1]]
+    K_normal=np.array(K).reshape(3,3)
+
+    # Descomponiendo E para obtener R y T
+    # descomposición en valores singulares o SVD
+    Points, R,T,mask = cv2.recoverPose(E, upts1, upts2, K_normal)
+    print ("Matriz de Rotacion")
+    print (R)
+    print ("Matriz de Traslacion")
+    print (T)
+
+    # Haciendo las matrices de proyección: R-> izquierda; T-> derecha
+    proj_mat2 = np.hstack((R,T))
+
+    # Matriz de proyección de la primera camara en el origen
+    # Después de este proceso se obtiene una de las dos matrices de cámara, para conseguir
+    # la segunda matriz hay que asumir que una de las cámaras es fija y canónica (no tiene
+    # rotación ni traslación), la siguiente matriz representa a esta cámara:
+    proj_mat1 = np.array([ [1,0,0,0],
+                    [0,1,0,0],
+                    [0,0,1,0]])
+
+	# Cambiando las dimensiones de upts para la triangulación.
+    upts1_array = np.array(upts1).reshape(2,len(pts1))
+    upts2_array = np.array(upts2).reshape(2,len(pts2))
+
+
+    # Triangulacion
+    # Conversión de coordenadas homogéneas 4D en coordenadas de imagen
+    P_l = np.dot(K,  proj_mat1)
+    P_r = np.dot(K,  proj_mat2)
+
+    pts4d = cv2.triangulatePoints(P_l, P_r, upts1, upts2)
+
+    point_4d_nonHom = pts4d / np.tile(pts4d[-1, :], (4, 1))
+    point_3d = point_4d_nonHom[:3, :].T
+    #print point_3d
+
+    #Exportando a txt
+    #np.savetxt("points_3d.csv", point_3d, delimiter="," , usecols=np.arange(0,2))
+
+    #5 - Puntos de Salida en 3D
+    #Mostrar puntos 3D
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.set_xlabel('X Label')
+    ax.set_ylabel('Y Label')
+    ax.set_zlabel('Z Label')
+
+    for x, y, z in point_3d:
+        ax.scatter(x, y, z, c="g", marker="o")
+
+    plt.show()
+    print()
